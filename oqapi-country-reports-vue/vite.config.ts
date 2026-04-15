be12fa288923137d0e8b58bcfff71c2c586aa499
Valentin Böhmer <144217555+vboehmer@users.noreply.github.com>
2026-04-15 17:33:04 +0200

    Update vite.config.ts

--- oqapi-country-reports-vue/vite.config.ts
+++ oqapi-country-reports-vue/vite.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173
-  }
-})
-
-export default defineConfig({
   // This ensures assets (JS/CSS) load from the correct subfolder
   base: '/humanitarian_pages/oqapi-country-report/', 
   plugins: [vue()],
