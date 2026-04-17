3b219c2f65c4aff6d45e06c49a7dec1cf9602354
Valentin Böhmer <144217555+vboehmer@users.noreply.github.com>
2026-04-17 17:02:31 +0200

    fix: Change map layer references to 'h3_hexgrid'

--- oqapi-country-reports-vue/src/views/MainView.vue
+++ oqapi-country-reports-vue/src/views/MainView.vue
@@ -70,9 +70,9 @@ const map1Indicator = ref('');
 const map2Indicator = ref('currentness');
 const map3Indicator = ref('');
 
-const map1Layer = ref('hexagonal_grid');
-const map2Layer = ref('hexagonal_grid');
-const map3Layer = ref('hexagonal_grid');
+const map1Layer = ref('h3_hexgrid');
+const map2Layer = ref('h3_hexgrid');
+const map3Layer = ref('h3_hexgrid');
 
 const schoolSwitchVisible = ref(false);
 const schoolSubTopic = ref('operator');
