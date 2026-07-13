4767cc450db7ee2fe567c2fe06324553392a30dc
vboehmer <valentin.boehmer@heigit.org>
2026-07-13 09:38:22 +0200

    refactor: remove unused stylesheet links from index.html and adjust imports in main.ts

--- gaia-viz-vue/src/main.ts
+++ gaia-viz-vue/src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./assets/styles/main.css";
-import App from "./App.vue";
-import router from "./router";
 import vuetify from "@/plugins/vuetify";
 import { registerGlobalComponents } from "@/plugins/globalComponents";
+import 'maplibre-gl/dist/maplibre-gl.css'
+import App from './App.vue'
+import router from './router'
 
 const pinia = createPinia();
 const app = createApp(App);
